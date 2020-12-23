57ae03240fb2d94c144ad04f2250cf33397567ff
Hoc Can Huynh <canhuynh1998@gmail.com>
2020-12-22 22:47:06 -0800

    Insert and Search Implementation

--- LinkedList/LinkedList.py
+++ LinkedList/LinkedList.py
@@ -11,15 +11,21 @@ class LinkedList:
         self.head = None
         self.length = 0
 
-    def search(self, value):
+    def search(self, target):
+        '''
+        Traverse through the list to find if the target value is in the list.
+        '''
         current = self.head
         while current is not None:
-            if current.value == value:
+            if current.value == target:
                 return True
             current = current.next
         return False
 
     def insertFront(self, value):
+        '''
+        Insertion at the beginning of the list.
+        '''
         if self.head is None:
             self.head = Node(value)
             self.length += 1
@@ -30,6 +36,9 @@ class LinkedList:
         self.length += 1
     
     def insertAfterANode(self, target, value):
+        '''
+        Insertion after the target Node.
+        '''
         if self.head is None:
             self.insertFront(value)
             return
@@ -39,25 +48,42 @@ class LinkedList:
         newNode = Node(value)
         newNode.next = target.next
         target.next = newNode
+        self.length += 1
 
-    def insertBetween(self, value, position):
+    def insertAtPosition(self, value, position):
+        '''
+        Insertion at the a specific position in the list
+        
+        By default, newNode will be add at the end of the list
+        when the position is greater than the current length of the list and head is not None.
+
+        If either head is None or position is 0, 
+        we will insert to the front of the list when head is not None else do nothing.
+        '''
         if position >= self.length and self.head is not None:
             self.insertBack(value)
             return
-        if position == 0 or self.head is None:
-            self.insertFront(value)
+        
+        if position == 0:
+            if self.head is not None:
+                self.insertFront(value)
             return
+            
         current = self.head
-        connect = self.head
         while position > 1:
             current = current.next
             connect = connect.next
             position -= 1
         insertNode = Node(value)
-        insertNode.next = current.next
+        insertNode.next = current.next if current.next is not None else None
         current.next = insertNode
+        self.length += 1
+    
     
     def insertBack(self, value):
+        '''
+        Insert at the end of the list
+        '''
         if self.head is None:
             self.insertFront(value)
             return
@@ -67,3 +93,4 @@ class LinkedList:
         
         newNode = Node(value)
         current.next = newNode
+        self.length += 1
