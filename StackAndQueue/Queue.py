b3156c6891325292342193d3f6c3c1d8b7c0debf
Hoc Can Huynh <canhuynh1998@gmail.com>
2020-12-23 23:35:40 -0800

    Queue Implementation using Linked List

--- StackAndQueue/Queue.py
+++ StackAndQueue/Queue.py
@@ -9,14 +9,26 @@ class Node:
     
 class Queue:
     def __init__(self):
-        self.head = None
-        self.tail = None
+        self.head = None    # For dequeue
+        self.tail = None    # For enqueue
         self.length = 0
 
     def isEmpty(self):
+        '''
+        Check if the queue is empty
+        '''
         return self.length == 0
     
+    def queueLength(self):
+        '''
+        Return the length of the queue
+        '''
+        return self.length 
+
     def enqueue(self, value):
+        '''
+        Insert at the end of the linked list
+        '''
         newNode = Node(value)
         if self.isEmpty():
             self.head = self.tail = newNode
@@ -26,10 +38,15 @@ class Queue:
         self.length += 1
     
     def dequeue(self):
+        '''
+        Remove at the beginning of the linked list
+        '''
         if self.isEmpty():
             return None
         returnNode = self.head
         self.head = self.head.next
+
+        # This will make sure we have an empty list if we remove the last node from the list
         if self.head is None:
             self.tail = None
         self.length -= 1
