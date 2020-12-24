2a4fc78a7914b999feaffa2d55aa5563543fe479
Hoc Can Huynh <canhuynh1998@gmail.com>
2020-12-23 23:22:50 -0800

    Stack Implementation using LinkedList

--- StackAndQueue/Stack.py
+++ StackAndQueue/Stack.py
@@ -7,33 +7,57 @@ class Node:
         self.value = value
         self.next = None
 
-
 class Stack:
     def __init__(self):
         self.head = None
         self.length = 0
 
     def push(self, value):
+        '''
+        Insert at the beginning of the linked List
+        '''
         newNode = Node(value)
-        if self.head is None:
+        if self.isEmpty():
             self.head = newNode
         else:
-            self.tail.next = newNode
+            newNode.next = self.head
+            self.head = newNode
         self.length += 1
 
     def pop(self):
+        '''
+        Remove and retrieve the first node in the linked list
+        '''
+        if self.isEmpty():
+            return None
         returnNode = self.head
         self.head = self.head.next 
+        returnNode.next = None
         self.length -= 1
-        return returnNode if returnNode else None
+        return returnNode 
     
     def isEmpty(self):
+        '''
+        Check if the stack is empty
+        '''
         return self.length == 0
 
     def peek(self):
-        print(self.head.value)
+        '''
+        Retrieve the first node in the linked list but not removing it
+        '''
+        return self.head if not self.isEmpty() else None
+
+    def stackLength(self):
+        '''
+        Return the length of the stack
+        '''
+        return self.length
 
     def search(self, value):
+        '''
+        Search for the position of the target value
+        '''
         if isEmpty:
             return -1
         current = self.head
